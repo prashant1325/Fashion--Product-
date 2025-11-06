752f05c7bb4ce8136d89aaf65f38271f578f4b0c
Prashant Kumar <puish9988@gmail.com>
2025-11-06 19:41:33 +0530

    Fix Sell model import case sensitivity

--- backend/routes/sell.js
+++ backend/routes/sell.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 // const Sell = require('../models/Sell');
-const Sell = require("../models/Sell");
+const Sell = require('../models/Sell');
+
 
 
 // GET all sell products
